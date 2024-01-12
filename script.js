c041f9cc0e2c62e5266e713303e6788152e1c99f
Adrian Aguilar <132902881+adriiag8@users.noreply.github.com>
2024-01-12 06:02:21 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,45 +1,50 @@
-const playerText = document.querySelector("#playerText");
-const computerText = document.querySelector("#computerText");
-const resultText = document.querySelector("#resultText");
-const choiceBtns = document.querySelectorAll(".choiceBtns");
+const choices = ["Rock", "Paper", "Scissors"];
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay = document.getElementById("computerDisplay");
+const resultDisplay = document.getElementById("resultDisplay");
+const playerScoreDisplay = document.getElementById("playerScoreDisplay");
+const computerScoreDisplay = document.getElementById("computerScoreDisplay");
 
-let player;
-let computer;
-let result;
+let playerScore = 0;
+let computerScore = 0;
 
-choiceBtns.forEach(button => button.addEventListener("click", () => {
+function playGame(playerChoice) {
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
 
-    player = button.textContent;
-    computerTurn();
-    playerText.textContent = `Player: ${player}`;
-    computerText.textContent = `Computer: ${computer}`;
-    resultText.textContent = checkWinner();
-}))
 
-function computerTurn() {
-    const random = Math.floor(Math.random() * 3) + 1;
+    let result = "";
 
-    switch (random) {
-        case 1:
-            computer = "Rock";
+    if (playerChoice === computerChoice) {
+        result = "WOW, You Tie!";
+    }
+    else {
+        switch (playerChoice) {
+            case "Rock":
+                result = (computerChoice === "Scissors") ? "You Win!" : "You Lose!";
+                break;
+            case "Paper":
+                result = (computerChoice === "Rock") ? "You Win!" : "You Lose!";
+                break;
+            case "Scissors":
+                result = (computerChoice === "Paper") ? "You Win!" : "You Lose!";
+                break;
+        }
+    }
+
+    playerDisplay.textContent = `Player: ${playerChoice}`;
+    computerDisplay.textContent = `Computer: ${computerChoice}`;
+    resultDisplay.textContent = `Result: ${result}`;
+
+    switch (result) {
+        case "You Win!":
+            playerScore++;
+            playerScoreDisplay.textContent = `Player Score: ${playerScore}`;
             break;
-        case 2:
-            computer = "Paper";
-            break;
-        case 3:
-            computer = "Scissors";
+        case "You Lose!":
+            computerScore++;
+            computerScoreDisplay.textContent = `Computer Score: ${computerScore}`;
             break;
     }
+
 }
 
-function checkWinner() {
-    if (player == computer) {
-        return "WOW, You Draw!";
-    } else if (computer == "Rock") {
-        return (player == "Paper") ? "You Win!" : "You Lose!"
-    } else if (computer == "Paper") {
-        return (player == "Scissors") ? "You Win!" : "You Lose!"
-    } else if (computer == "Scissors") {
-        return (player == "Rock") ? "You Win!" : "You Lose!"
-    }
-}
